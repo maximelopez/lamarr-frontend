7f40f35eda31cd2138eadd7dc4a7f5d39b6e85d4
Maxime <maxime.lpz@outlook.fr>
2026-07-09 09:40:55 +0200

    mise en place tailwindcss

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,9 @@
 export default function Dashboard() {
   return (
-    <div className="page">
-      <h1>Dashboard</h1>
+    <div className="bg-base p-8">
+      <span className="text-tech font-bold">Tech</span>{" "}
+      <span className="text-ia font-bold">IA</span>{" "}
+      <span className="text-entreprenariat font-bold">Entreprenariat</span>
     </div>
   );
 }
